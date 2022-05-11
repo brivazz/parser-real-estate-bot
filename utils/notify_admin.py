78e7b2540951b2efc013f2e2b80dffa62da1b8a9
brivazz <yateperna@gmail.com>
2022-05-11 18:27:13 +0300

    Добавил utils/notify_admin

---
+++ utils/notify_admin.py
@@ -0,0 +1,14 @@
+import logging
+from utils.config import MY_ID
+
+
+async def on_startup_notify(dp):
+    if MY_ID:
+        try:
+            await dp.bot.send_message(
+                text='Бот запущен',
+                chat_id=MY_ID,
+                disable_notification=True
+            )
+        except Exception as err:
+            logging.error(err)
